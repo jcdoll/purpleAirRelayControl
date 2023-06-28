2065f9b09d4843b7b185a408d30116b3a5c622a5
Joey Doll <jcdoll@gmail.com>
2023-06-28 14:53:36 -0700

    added sample secrets file

---
+++ arduino_secrets_sample.h
@@ -0,0 +1,4 @@
+#define SECRET_SSID "your_wifi_ssid"
+#define SECRET_PASS "your_password"
+#define SECRET_PURPLE_AIR_KEY "3B77GF5C-A051-31EC-4562-49110F800005"
+String SECRET_SENSOR_IDS[2] = {"112800", "98029"};
